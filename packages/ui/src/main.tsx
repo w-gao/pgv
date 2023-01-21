5afc93ad04d4d02296b929b3c00e55d53d8fe52c
William Gao <williamgao520@gmail.com>
2023-01-21 13:18:50 -0800

    pull in three.js

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,10 +1,32 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
 import "./index.css"
+import { StrictMode } from "react"
+import { createRoot, events, extend } from "@react-three/fiber"
+import { SVGRenderer } from "three/examples/jsm/renderers/SVGRenderer"
+import TorusKnot from "./components/TorusKnot"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+import * as THREE from "three"
+extend(THREE)
+
+const root = createRoot(document.getElementById("root") as HTMLElement)
+root.configure({
+    gl: canvas => {
+        const gl = new SVGRenderer()
+        canvas.appendChild(gl.domElement)
+        return gl
+    },
+    frameloop: "demand",
+    events,
+    camera: { position: [0, 0, 50] },
+    size: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        top: 0,
+        left: 0,
+    },
+})
+
+root.render(
+    <StrictMode>
+        <TorusKnot />
+    </StrictMode>
 )
