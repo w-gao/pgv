a3db1ea761f5ffc54e5c56e08069444ad0247189
William Gao <williamgao520@gmail.com>
2023-03-07 21:03:55 -0800

    consolidate pgv add and update

--- cli.py
+++ cli.py
@@ -14,8 +14,7 @@ pgv CLI.
 Available commands:
 
 - pgv ls
-- pgv add
-- pgv update
+- pgv [add|update]
 - pgv rm
 
 
@@ -62,80 +61,16 @@ class PgvCLI:
 
     def cmd_ls(self) -> None:
         """
-        The list command.
+        The "list" command.
         """
         print("IDENTIFIER\tREGION")
         for src in self.sources:
             print(f"{src.get('identifier')}\t\t{src.get('region')}")
         print(self.args)
 
-    def cmd_update(self) -> None:
-        """
-        The update command.
-        """
-        args = self.args
-        identifier = args.identifier
-
-        src: Dict[str, str] = {}
-        for s in self.sources:
-            if s.get("identifier") == identifier:
-                src = s
-                break
-        else:
-            logger.warning(f"{identifier} does not exist in registry.")
-            exit()
-
-        # The xg file with the entire graph
-        xgFile = f"{identifier}.xg"
-
-        dir = os.path.join(self.cwd, identifier)
-        if not os.path.exists(dir):
-            logger.warning(f"{identifier} does not exist.")
-            exit()
-
-        # Chunk the file based on region
-        if args.node_range:
-            cmd = ["vg", "chunk", "-x", os.path.join(dir, xgFile), "-c", str(args.context_step), "-r", args.node_range]
-            if args.gbwt:
-                cmd.extend(["--gbwt-name", args.gbwt])
-            cmd.extend(["-T", "-b", os.path.join(dir, "chunk"), "-E", os.path.join(dir, "regions.tsv")])
-
-            xgFile = f"{identifier}.chunked.xg"
-            logger.info("Running subprocess command: '%s'", " ".join(cmd))
-            with open(os.path.join(dir, xgFile), "w") as f:
-                popen = subprocess.Popen(cmd, cwd=os.getcwd(), stdout=f, stderr=subprocess.PIPE)
-                _, stderr = popen.communicate()
-            if popen.returncode != 0:
-                # "vg chunk" failed
-                # TODO: xgFile would've been overwritten.
-                logger.warning("ERROR: 'vg chunk' failed:", stderr)
-                exit(1)
-
-        # Get a JSON graph for pgv to interpret.
-        cmd = ["vg", "view", "-j", os.path.join(dir, xgFile)]
-        jsonFile = f"{xgFile}.json"
-        with open(os.path.join(dir, jsonFile), "w") as f:
-            popen = subprocess.Popen(cmd, cwd=os.getcwd(), stdout=f, stderr=subprocess.PIPE)
-            _, stderr = popen.communicate()
-        if popen.returncode != 0:
-            # "vg index" failed
-            # TODO: xgFile would've been overwritten.
-            logger.warning("ERROR: 'vg index' failed:", stderr)
-            exit(1)
-
-        # Update sources.json.
-        if args.display_name:
-            src["name"] = args.display_name
-        src["xgFile"] = xgFile
-        src["region"] = args.node_range
-        src["jsonFile"] = jsonFile
-
-        self._update_source()
-        logger.info("SUCCESS")
-
     def cmd_rm(self) -> None:
         """
-        The remove command.
+        The "remove" command.
         """
         args = self.args
         identifier = args.identifier
@@ -163,51 +98,59 @@ class PgvCLI:
 
     def cmd_add(self) -> None:
         """
-        The add command.
+        The "add" and "update" command.
         """
         args = self.args
         identifier = args.identifier
-
         dir = os.path.join(self.cwd, identifier)
-        if os.path.exists(dir):
-            logger.warning(f"ERROR: '{identifier}' exists.")
-            exit(1)
-        os.mkdir(dir)
+
+        if not (exists := os.path.exists(dir)):
+            # We are creating a new graph.
+            os.mkdir(dir)
+
+        src: Dict[str, str] = {}
+        if exists:
+            for s in self.sources:
+                if s.get("identifier") == identifier:
+                    src = s
+                    break
+            else:
+                logger.warning(f"{identifier} does not exist in registry.")
+                exit(1)
 
         references = args.reference
         vcfs = args.vcf
 
-        if not references:
+        if not exists and not references:
             logger.warning("ERROR: at least one FASTA file must be provided.")
             exit(1)
 
-        # Construct .vg file.
-        cmd = ["vg", "construct"]
-        for r in references:
-            cmd.extend(["-r", r])
-        for v in vcfs:
-            cmd.extend(["-v", v])
-
-        with open(f"{dir}/{identifier}.vg", "w") as f:
-            popen = subprocess.Popen(cmd, cwd=os.getcwd(), stdout=f, stderr=subprocess.DEVNULL)
-            popen.communicate()
-        if popen.returncode != 0:
-            # "vg construct" failed
-            shutil.rmtree(dir)
-            logger.warning("ERROR: 'vg construct' failed")
-            exit(1)
-
-        # Index .vg to get .xg file.
+        vgFile = f"{identifier}.vg"
         xgFile = f"{identifier}.xg"
+        if references:
+            # Construct .vg file.
+            cmd = ["vg", "construct"]
+            for r in references:
+                cmd.extend(["-r", r])
+            for v in vcfs:
+                cmd.extend(["-v", v])
 
-        cmd = ["vg", "index", f"{dir}/{identifier}.vg", "-x", os.path.join(dir, xgFile)]
-        popen = subprocess.Popen(cmd, cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        _, stderr = popen.communicate()
-        if popen.returncode != 0:
-            # "vg index" failed
-            shutil.rmtree(dir)
-            logger.warning("ERROR: 'vg index' failed:", stderr)
-            exit(1)
+            with open(os.path.join(dir, vgFile), "w") as f:
+                popen = subprocess.Popen(cmd, cwd=os.getcwd(), stdout=f, stderr=subprocess.DEVNULL)
+                popen.communicate()
+            if popen.returncode != 0:
+                # "vg construct" failed
+                logger.warning("ERROR: 'vg construct' failed")
+                exit(1)
+
+            # Index .vg to get .xg file.
+            cmd = ["vg", "index", os.path.join(dir, vgFile), "-x", os.path.join(dir, xgFile)]
+            popen = subprocess.Popen(cmd, cwd=os.getcwd(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            _, stderr = popen.communicate()
+            if popen.returncode != 0:
+                # "vg index" failed
+                logger.warning("ERROR: 'vg index' failed:", stderr)
+                exit(1)
 
         # Chunk the file based on region
         if args.node_range:
@@ -222,7 +165,6 @@ class PgvCLI:
                 _, stderr = popen.communicate()
             if popen.returncode != 0:
                 # "vg chunk" failed
-                shutil.rmtree(dir)
                 logger.warning("ERROR: 'vg chunk' failed:", stderr)
                 exit(1)
 
@@ -234,18 +176,21 @@ class PgvCLI:
             _, stderr = popen.communicate()
         if popen.returncode != 0:
             # "vg index" failed
-            shutil.rmtree(dir)
             logger.warning("ERROR: 'vg index' failed:", stderr)
             exit(1)
 
-        # Add this to sources.json.
-        self.sources.append(dict(
-            identifier=identifier,
-            name=self.args.display_name if self.args.display_name else identifier,
-            xgFile=xgFile,
-            region = args.node_range,
-            jsonFile = jsonFile
-        ))
+        src["identifier"] = identifier
+        if args.display_name:
+            src["name"] = args.display_name
+        elif not exists:
+            src["name"] = identifier
+        src["xgFile"] = xgFile
+        src["region"] = args.node_range
+        src["jsonFile"] = jsonFile
+
+        if not exists:
+            # Add this to sources.json.
+            self.sources.append(src)
 
         self._update_source()
         logger.info("SUCCESS")
@@ -260,25 +205,17 @@ def main(args: List[str]) -> None:
     # "ls" command
     ls_parser = subparsers.add_parser("ls", help="List all graphs.")
 
-    # "add" command
-    add_parser = subparsers.add_parser("add", help="Add a new graph.")
+    # "add" and "update" command
+    add_parser = subparsers.add_parser("add", aliases=["update"], help="Add or update a graph.")
     add_parser.add_argument("identifier")
     add_parser.add_argument("-n", "--name", dest="display_name", help="display name")
     # construct args
-    add_parser.add_argument("-f", "--reference", metavar="FILE", action="extend", nargs="+", help="input FASTA reference file(s)")
+    add_parser.add_argument("-f", "--reference", metavar="FILE", action="extend", nargs="*", help="input FASTA reference file(s)")
     add_parser.add_argument("-v", "--vcf", metavar="FILE", action="extend", nargs="*", help="input VCF file(s)")
     # chunk args
     add_parser.add_argument("-r", "--node-range", dest="node_range", metavar="N:M", help="the node range")
     add_parser.add_argument("-c", "--context-step", dest="context_step", type=int, metavar="N", help="expand N node steps as content", default=20)
     add_parser.add_argument("-G", "--gbwt-name", dest="gbwt", metavar="FILE", help="input GBWT file")
-
-    # "update" command
-    update_parser = subparsers.add_parser("update", help="Update the chunk range of an existing graph.")
-    update_parser.add_argument("identifier")
-    update_parser.add_argument("-n", "--name", dest="display_name", help="display name")
-    update_parser.add_argument("-r", "--node-range", dest="node_range", metavar="N:M", help="the node range")
-    update_parser.add_argument("-c", "--context-step", dest="context_step", type=int, metavar="N", help="expand N node steps as content", default=20)
-    update_parser.add_argument("-G", "--gbwt-name", dest="gbwt", metavar="FILE", help="input GBWT file")
 
     # "rm" command
     rm_parser = subparsers.add_parser("rm", help="Remove a graph.")
@@ -292,10 +229,8 @@ def main(args: List[str]) -> None:
 
     if options.cmd == "ls":
         cli.cmd_ls()
-    elif options.cmd == "add":
+    elif options.cmd in ("add", "update"):
         cli.cmd_add()
-    elif options.cmd == "update":
-        cli.cmd_update()
     elif options.cmd == "rm":
         cli.cmd_rm()
     else:
