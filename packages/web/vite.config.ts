9f010892eb06f8ab82332fb0cb2c6f54d5cfc147
William Gao <williamgao520@gmail.com>
2023-04-03 21:26:23 -0700

    chore: split vendor code in build process

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [preact(), splitVendorChunkPlugin()],
 })
